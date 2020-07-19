2161100c3df76e2a57e00117e8d14320fbb6d899
sameedullah <sameedu786@gmail.com>
2020-07-19 18:00:11 +0500

    routine work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
 let currentResult = 0;
 currentResult = currentResult + 10 * 3;
 
-let calculationDiscription = "0 + 10 * 3"
+let calculationDiscription = ""+ currentResult +" + 10 * 3"
 
 outputResult(currentResult,calculationDiscription);
