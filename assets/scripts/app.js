38e4060c55d70a4e723aeb16a2762ffb83af2e63
sameedullah <sameedu786@gmail.com>
2020-07-20 02:44:26 +0500

    routine work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,6 +10,7 @@ function add(num1,num2) {
 currentResult = (1,2);
 currentResult = (1,2);
 currentResult = (1,2);
+currentResult = (1,2);
 
 
 let calculationDiscription = ""+ defaultReslut +" + 10 * 3";
