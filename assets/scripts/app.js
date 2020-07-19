bcd8831d6e2779096edf83eb755435a33536b401
sameedullah <sameedu786@gmail.com>
2020-07-20 02:43:31 +0500

    routine work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,7 +8,6 @@ function add(num1,num2) {
 }
 
 currentResult = (1,2);
-currentResult = (1,2);
 
 let calculationDiscription = ""+ defaultReslut +" + 10 * 3";
 /* <<<<<<<  this only works with "`" backticks not single or double quotes and this syntax is called Template literals (Template strings) >>>>>>>
