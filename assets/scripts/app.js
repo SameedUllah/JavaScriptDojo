98110b307f27318a6d394cd2dbcf8e527ab48b62
sameedullah <sameedu786@gmail.com>
2020-07-19 18:33:52 +0500

    routine work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,8 +3,11 @@ let currentResult = defaultReslut;
 currentResult = currentResult + 10 * 3;
 
 let calculationDiscription = ""+ defaultReslut +" + 10 * 3"
-// <<<<<<<  this only works with "`" backticks not single or double quotes >>>>>>>
-// let calculationDiscription = `${defaultReslut} + 10 * 3` 
+// <<<<<<<  this only works with "`" backticks not single or double quotes and this syntax is called Template literals (Template strings) >>>>>>>
+/* let calculationDiscription = `${defaultReslut} + 
+
+ 10 * 3`*/ 
+ 
 // let calculationDiscription = `${10+10} + 10 * 3`
 
 outputResult(currentResult,calculationDiscription);
